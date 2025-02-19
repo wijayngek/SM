4adaaa3310e1b4b61bc191a166167e1ba1545b65
wijayngek <logroniojosepherwin@icloud.com>
2025-02-19 21:35:15 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// Show Content on Click
+function showContent(section) {
+    let sections = document.querySelectorAll('.content > div');
+    sections.forEach(sec => sec.classList.add('hidden'));
+    document.getElementById(section).classList.remove('hidden');
+}
+
+// Search Bar Functionality
+function searchFunction() {
+    let input = document.getElementById("searchBar").value.toLowerCase();
+    let links = document.querySelectorAll(".sidenav a");
+
+    links.forEach(link => {
+        if (link.textContent.toLowerCase().includes(input)) {
+            link.style.display = "";
+        } else {
+            link.style.display = "none";
+        }
+    });
+}
+Sent
+Write to
